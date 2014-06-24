2f8d855a96248c284bea387b54f03a196d5f55bc
Philip Corliss <pcorliss@50projects.com>
2014-06-23 21:46:35 -0500

    I hate javascript

--- server.js
+++ server.js
@@ -1,14 +1,61 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var memjs = require('memjs');
+var mc = memjs.Client.create(null, {expires: 15});
+var h = require('http');
+
+var enabled = function(cb, fail) {
+  mc.get('features', function(err, valBuff) {
+    var features, rawJSON;
+    if(valBuff){
+      rawJSON = valBuff.toString('utf-8');
+      features = JSON.parse(rawJSON);
+    }
+    if(features && features.step2){
+      cb()
+    } else {
+      var options = {
+        host: 'vuln.alttab.org',
+        port: 80,
+        path: '/features'
+      };
+
+      h.get(options, function(res) {
+        var output = '';
+        res.on('data', function (chunk) {
+          output += chunk;
+        });
+
+        res.on('end', function() {
+          var newFeatures = JSON.parse(output);
+          mc.set('features', output, function(){});
+          if(newFeatures && newFeatures.step2){
+            cb();
+          } else {
+            fail();
+          }
+        });
+      });
+    }
+  });
+};
 
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+  enabled(function(){
+    res.sendfile('index.html');
+  },function(){
+    res.sendfile('fail.html');
+  });
 });
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+    enabled(function(){
+      io.emit('chat message', msg);
+    }, function(){
+      io.emit('chat message', 'Lost to the ether');
+    });
   });
 });
 
